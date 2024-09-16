f9dca89c504c2120e5c581f409d042dfae51d776
Dumitru <dima50066@gmail.com>
2024-09-16 17:48:46 +0200

    work with db

---
+++ src/db/initMongoDB.js
@@ -0,0 +1,22 @@
+// src/db/initMongoDB.js
+
+import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
+
+export const initMongoDB = async () => {
+  try {
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PWD');
+    const url = env('MONGODB_URL');
+    const dv = env('MONGODB_DV');
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+    throw err;
+  }
+};
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { env } from './utils/env.js';
+import { getAllStudents, getStudentById } from './services/students.js';
 
 dotenv.config();
 
@@ -47,3 +48,25 @@ export const startServer = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
+
+app.get('/students', async (req, res) => {
+  const students = await getAllStudents();
+  res.status(200).json;
+  ({ data: students });
+});
+
+app.get('/students/:studentId', async (req, res) => {
+  const { studentId } = req.params;
+  const student = await getStudentById(studentId);
+
+  if (!student) {
+    res.status(404).json({
+      message: 'Student not found',
+    });
+    return;
+  }
+
+  res.status(200).json({
+    data: student,
+  });
+});
