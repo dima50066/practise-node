d85eeee4ae5487c5866be3e1b400c1669ad78d34
Dumitru <dima50066@gmail.com>
2024-09-25 16:44:59 +0200

    work with reset email

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,6 +5,7 @@ import {
   loginUser,
   logoutUser,
   refreshUsersSession,
+  requestResetToken,
 } from '../services/auth.js';
 
 import { ONE_DAY } from '../constants/index.js';
@@ -74,5 +75,14 @@ export const refreshUserSessionController = async (req, res) => {
     data: {
       accessToken: session.accessToken,
     },
+  });
+};
+
+export const requestResetEmailController = async (req, res) => {
+  await requestResetToken(req.body.email);
+  res.json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
   });
 };
